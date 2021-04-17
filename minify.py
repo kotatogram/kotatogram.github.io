41aef0119e5d667ade575fb5825f068fd1807283
RadRussianRus <radrussianrus@gmail.com>
2021-04-17 14:10:35 +0300

    Minify JSONs

---
+++ minify.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import json
+
+for path, _, names in os.walk("public"):
+    for name in names:
+        if not name.endswith(".json"):
+            continue
+
+        with open("{}/{}".format(path, name), "r", encoding="utf-8") as f:
+            data = json.load(f)
+
+        with open("{}/{}".format(path, name), "w", encoding="utf-8") as f:
+            json.dump(data, f)
